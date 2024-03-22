c2f3a3e4a9b092a1a9ef5003bdcc088e81fc2b5b
Fabian <fabiananguiano@gmail.com>
2024-03-22 16:51:13 -0500

    Add files via upload

---
+++ qr_button_gen/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': "Product QR Code Generator",
+    'version': "1.0",
+    'depends': ['base', 'product'],
+    'author': "fabiananguiano.com",
+    'category': "Product",
+    'description': """
+    Module adds a button to generate QR code for product.
+    """,
+    'data': [
+        'views/product_template_qr_view.xml',
+       
+    ],
+    'images': ['static\description\Banner.png'],
+    'installable': True,
+    'auto_install': False,
+    'license': 'OPL-1'
+}
